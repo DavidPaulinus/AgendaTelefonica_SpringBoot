a29cf6b7b09c97f273d07d0360fc877e45da9b6c
DavidPaulinus <davipm@hotmail.com>
2023-02-01 09:13:49 -0300

    projeto criado

---
+++ src/main/java/br/com/agenda/AgendaTelefonica_SpringBoot/AgendaTelefonicaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.agenda.AgendaTelefonica_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaTelefonicaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaTelefonicaSpringBootApplication.class, args);
+	}
+
+}
